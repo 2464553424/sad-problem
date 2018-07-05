ba6a9d258f1bc19d774d3288ad18bb75fc928cfb
2464553424 <2464553424@qq.com>
2018-07-05 16:25:11 +0800

    test

---
+++ sadproblem/测试/testViewController.m
@@ -0,0 +1,38 @@
+//
+//  testViewController.m
+//  sadproblem
+//
+//  Created by Fly on 2018/7/5.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import "testViewController.h"
+
+@interface testViewController ()
+
+@end
+
+@implementation testViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"测试");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
