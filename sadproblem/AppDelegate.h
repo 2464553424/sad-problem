283f82e82324d85b9c2228eb0a6e51736cc41596
2464553424 <2464553424@qq.com>
2018-07-05 15:58:41 +0800

    Initial Commit

---
+++ sadproblem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sadproblem
+//
+//  Created by Fly on 2018/7/5.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
