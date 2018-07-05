90c76f58ce220271222a197b0fd40b51bebe2d99
2464553424 <2464553424@qq.com>
2018-07-05 16:32:24 +0800

    是的

---
+++ sadproblem/文件/asdViewController.h
@@ -0,0 +1,13 @@
+//
+//  asdViewController.h
+//  sadproblem
+//
+//  Created by Fly on 2018/7/5.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface asdViewController : UIViewController
+
+@end
---
+++ sadproblem/文件/asdViewController.m
@@ -0,0 +1,38 @@
+//
+//  asdViewController.m
+//  sadproblem
+//
+//  Created by Fly on 2018/7/5.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import "asdViewController.h"
+
+@interface asdViewController ()
+
+@end
+
+@implementation asdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"   文件");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
